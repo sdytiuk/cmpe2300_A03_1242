24f084d1c1afd82f71a941171f5c65d7228579cf
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2025-01-09 09:53:35 -0700

    demo1

---
+++ Demo1/Demo1/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Demo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScroll = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScroll
+            // 
+            this.lblScroll.BackColor = System.Drawing.Color.Red;
+            this.lblScroll.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScroll.Location = new System.Drawing.Point(13, 13);
+            this.lblScroll.Name = "lblScroll";
+            this.lblScroll.Size = new System.Drawing.Size(341, 97);
+            this.lblScroll.TabIndex = 0;
+            this.lblScroll.Text = "label1";
+            this.lblScroll.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScroll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScroll;
+    }
+}
+
---
+++ Demo1/Demo1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo1
+{
+    public partial class Form1 : Form
+    {
+        int initialX;
+        int value;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Demo1";
+            lblScroll.Text = "Click and drag";
+            lblScroll.MouseDown += LblScroll_MouseDown;
+            lblScroll.MouseMove += LblScroll_MouseMove;
+            lblScroll.MouseUp += LblScroll_MouseUp;
+        }
+
+        private void LblScroll_MouseUp(object sender, MouseEventArgs e)
+        {
+            lblScroll.Capture = false;
+        }
+
+        private void LblScroll_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!lblScroll.Capture)
+                return;
+            value = (e.X - initialX)/10;
+            lblScroll.Text = value.ToString();
+        }
+
+        private void LblScroll_MouseDown(object sender, MouseEventArgs e)
+        {
+            lblScroll.Capture = true;
+            initialX = e.X;
+        }
+    }
+}

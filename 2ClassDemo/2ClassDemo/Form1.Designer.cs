699c76fee146295d30544156f459f2da7fa94540
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2025-01-13 14:36:19 -0700

    demo complete

---
+++ 2ClassDemo/2ClassDemo/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace _2ClassDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewCar = new System.Windows.Forms.Button();
+            this.btnShowCars = new System.Windows.Forms.Button();
+            this.lbCars = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnNewCar
+            // 
+            this.btnNewCar.Location = new System.Drawing.Point(13, 13);
+            this.btnNewCar.Name = "btnNewCar";
+            this.btnNewCar.Size = new System.Drawing.Size(75, 23);
+            this.btnNewCar.TabIndex = 0;
+            this.btnNewCar.Text = "New Car";
+            this.btnNewCar.UseVisualStyleBackColor = true;
+            // 
+            // btnShowCars
+            // 
+            this.btnShowCars.Location = new System.Drawing.Point(13, 43);
+            this.btnShowCars.Name = "btnShowCars";
+            this.btnShowCars.Size = new System.Drawing.Size(75, 23);
+            this.btnShowCars.TabIndex = 1;
+            this.btnShowCars.Text = "Show Cars";
+            this.btnShowCars.UseVisualStyleBackColor = true;
+            // 
+            // lbCars
+            // 
+            this.lbCars.FormattingEnabled = true;
+            this.lbCars.Location = new System.Drawing.Point(140, 13);
+            this.lbCars.Name = "lbCars";
+            this.lbCars.Size = new System.Drawing.Size(184, 368);
+            this.lbCars.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbCars);
+            this.Controls.Add(this.btnShowCars);
+            this.Controls.Add(this.btnNewCar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewCar;
+        private System.Windows.Forms.Button btnShowCars;
+        private System.Windows.Forms.ListBox lbCars;
+    }
+}
+
---
+++ 2ClassDemo/2ClassDemo/Form1.cs
@@ -0,0 +1,104 @@
+﻿/* Creating a new class
+ * Steven Dytiuk
+ *  */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2ClassDemo
+{
+    
+    public partial class Form1 : Form
+    {
+        List<Car> cars = null;
+        public Form1()
+        {
+            InitializeComponent();
+            btnNewCar.Click += BtnNewCar_Click;
+            btnShowCars.Click += BtnShowCars_Click;
+            lbCars.Click += LbCars_Click;
+        }
+
+        private void LbCars_Click(object sender, EventArgs e)
+        {
+            ((Car)((ListBox)sender).SelectedItem).RenovateCar();
+        }
+
+        private void BtnShowCars_Click(object sender, EventArgs e)
+        {
+            FirstCar();
+            lbCars.DataSource = null;
+            lbCars.DataSource = cars;
+        }
+
+        private void FirstCar()
+        {
+            cars?[0].ShowYourself();
+
+            int? x = null;
+            if (x == null) x = 3;
+            Console.WriteLine(x);
+
+        }
+
+        private void BtnNewCar_Click(object sender, EventArgs e)
+        {
+            if (cars == null)
+                cars = new List<Car>();
+            cars.Add( new Car());
+            
+        }
+    }
+
+    public class Car
+    {
+        private string CarType;
+        private int YearBuilt;
+        Random rand = new Random();
+
+        //constructor with params
+        public Car(string t, int y)
+        {
+            CarType = t;
+            YearBuilt = y;
+        }
+
+        //leverage your constructor with params
+        public Car(string t) : this(t, 2023)
+        {
+        }
+
+        //default constructor (no params)
+        public Car()
+        {
+            List<string> cartypes = new List<string>();
+            cartypes.Add("Mazda");
+            cartypes.Add("Ford");
+            cartypes.Add("Chevy");
+            CarType = cartypes[rand.Next(cartypes.Count)];
+            YearBuilt = rand.Next(1980, 2026);
+        }
+
+        public override string ToString()
+        {
+            return YearBuilt + " " + CarType;
+        }
+
+        public void RenovateCar()
+        {
+            YearBuilt++;
+        }
+
+        public void ShowYourself()
+        {
+            Console.WriteLine(this);
+        }
+
+    }
+}
